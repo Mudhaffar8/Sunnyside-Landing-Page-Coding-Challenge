5dbe6a77b829a955f386bde70d9b2c83886158d0
Mudhaffar8 <139074756+Mudhaffar8@users.noreply.github.com>
2023-09-17 17:23:23 -0400

    Fixed Nav bar bug

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function toggle_nav() {
         nav.classList.remove('main-nav__links--hide');
     }
 }
+
+function resizeFn() {
+    if (window.innerWidth >= 751 && (nav.classList.contains('main-nav__links--show') || 
+        nav.classList.contains('main-nav__links--hide'))) {
+        nav.classList.remove('main-nav__links--show');
+        nav.classList.remove('main-nav__links--hide');
+    }
+}
+
+window.onresize = resizeFn;
+resizeFn();
+
